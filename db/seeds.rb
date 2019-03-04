c61e3fc53cb9e2bd57a5c29b6d24eee10f363786
Kumi Takenaka <kumireliy@gmail.com>
2019-03-04 17:09:38 +0900

    added genre to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+blues = Genre.new(name: 'Blues')
+blues.save!
+electronic = Genre.new(name: 'Electronic')
+electronic.save!
+hip_hop = Genre.new(name: 'Hip hop')
+hip_hop.save!
+classic = Genre.new(name: 'Classic')
+classic.save!
+rock = Genre.new(name: 'Rock')
+rock.save!
+pop = Genre.new(name: 'Pop')
+pop.save!
+jazz = Genre.new(name: 'Jazz')
+jazz.save!
+folk = Genre.new(name: 'Folk')
+folk.save!
+country = Genre.new(name: 'Country')
+country.save!
+reggae = Genre.new(name: 'Reggae')
+reggae.save!
+punk_rock = Genre.new(name: 'Punk Rock')
+punk_rock.save!
+heavy_metal = Genre.new(name: 'Heavy Metal')
+heavy_metal.save!
+soul = Genre.new(name: 'Soul')
+soul.save!
+dance = Genre.new(name: 'Dance')
+dance.save!
+funk = Genre.new(name: 'Funk')
+funk.save!
+disco = Genre.new(name: 'Disco')
+disco.save!
+techno = Genre.new(name: 'Techno')
+techno.save!
+house = Genre.new(name: 'House')
+house.save!
+trance = Genre.new(name: 'Trance')
+trance.save!
+latino = Genre.new(name: 'Latino')
+latino.save!
+ska = Genre.new(name: 'Ska')
+ska.save!
