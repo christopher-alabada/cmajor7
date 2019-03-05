694f31ed702324d54adcee67c1aa270cc37eb9be
Kumi Takenaka <kumireliy@gmail.com>
2019-03-05 11:27:00 +0900

    added bands controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bands/index'
+  get 'bands/show'
   devise_for :musicians
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
