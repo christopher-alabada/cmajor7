ab025bd40fc2e4aa8a9262bb126d968cadc4afb9
Yuki Mori <masked04@gmail.com>
2019-03-05 12:47:51 +0900

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_031030) do
+ActiveRecord::Schema.define(version: 2019_03_05_031113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,9 +78,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_031030) do
     t.string "location"
     t.string "years_experience"
     t.string "tag_line"
-    t.text "descrption"
     t.string "user_photo"
     t.string "banner_photo"
+    t.text "description"
     t.index ["email"], name: "index_musicians_on_email", unique: true
     t.index ["reset_password_token"], name: "index_musicians_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -63,22 +63,22 @@ song = Song.new(title: 'Autumn leaves', artist: 'Bill Evans')
 
 musician = Musician.new(fullname: 'Kumi T', email: 'kumit@example.com', password: 'pass_kumi',
   username: 'kumit', date_of_birth: DateTime.new(1990, 3, 2), location: 'Osaka', years_experience: '3 years', tag_line: 'Hello', descrption: 'Hi guys, I hope we can have a great time!', user_photo:  )
+musician.user_photo = 'https://www.designskilz.com/random-users/images/imageF31.jpg'
+musician.banner_photo = 'https://images.unsplash.com/photo-1462813277085-db5366eb347b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
 musician.save!
-musician.user_photo_url = 'https://www.designskilz.com/random-users/images/imageF31.jpg'
-musician.banner_photo_url = 'https://images.unsplash.com/photo-1462813277085-db5366eb347b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
 
 musicians = Musician.new(
   username: 'Vic',
   full_name: 'Victor Wooten',
   email: 'victorwooten@example.com',
   password: 'pass_victor',
-  date_of_birth: DateTime.new(1964, 9, 11);
+  date_of_birth: DateTime.new(1964, 9, 11),
   location: 'Aoyama',
-  years_experience: 6,
+  years_experience: '6',
   tag_line: 'Music as a language',
   description: 'An American bass guitarist, record producer, educator, and recipient of five Grammy Awards.[2] He has been the bassist for Béla Fleck and the Flecktones since the groups formation in 1988 and a member of the band SMV with two other bassists, Stanley Clarke and Marcus Miller.[1][3] Since 2017 he has played bass for the metal band Nitro.',
-  user_photo_url: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
-  banner_photo_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
+  user_photo: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
+  banner_photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
   )
 musicians.save!
 
@@ -87,13 +87,13 @@ musicians = Musician.new(
   full_name: 'Victor Wooten',
   email: 'victorwooten@example.com',
   password: 'pass_victor',
-  date_of_birth: DateTime.new(1964, 9, 11);
+  date_of_birth: DateTime.new(1964, 9, 11),
   location: 'Aoyama',
-  years_experience: 6,
+  years_experience: '1',
   tag_line: 'Music as a language',
   description: 'An American bass guitarist, record producer, educator, and recipient of five Grammy Awards.[2] He has been the bassist for Béla Fleck and the Flecktones since the groups formation in 1988 and a member of the band SMV with two other bassists, Stanley Clarke and Marcus Miller.[1][3] Since 2017 he has played bass for the metal band Nitro.',
-  user_photo_url: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
-  banner_photo_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
+  user_photo: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
+  banner_photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
   )
 musicians.save!
 
@@ -102,18 +102,18 @@ musicians = Musician.new(
   full_name: 'Victor Wooten',
   email: 'yukimori@example.com',
   password: 'pass_yuki',
-  date_of_birth: DateTime.new(1964, 9, 11);
+  date_of_birth: DateTime.new(1964, 9, 11),
   location: 'Shinjuku',
-  years_experience: 6,
+  years_experience: ',
   tag_line: 'Music as a language',
   description: 'An American bass guitarist, record producer, educator, and recipient of five Grammy Awards.[2] He has been the bassist for Béla Fleck and the Flecktones since the groups formation in 1988 and a member of the band SMV with two other bassists, Stanley Clarke and Marcus Miller.[1][3] Since 2017 he has played bass for the metal band Nitro.',
-  user_photo_url: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
-  banner_photo_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
+  user_photo: 'https://en.wikipedia.org/wiki/File:Victor_Wooten.jpg',
+  banner_photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fres-3.cloudinary.com%2Fdostuff-media%2Fimage%2Fupload%2F%2Fc_fill%2Cg_faces%2Cf_auto%2Cw_800%2Fv1485794800%2Fevent-poster-7363238.jpg&imgrefurl=https%3A%2F%2Fdo512.com%2Fevents%2F2017%2F4%2F1%2Fthe-victor-wooten-trio-ft-dennis-chambers-bob-franceschini-w-henry-the-invisibles&docid=tgpGJfkJ6DTgEM&tbnid=LMglwZtq9kxlLM%3A&vet=12ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI..i&w=800&h=301&bih=766&biw=1440&q=victor%20wooten&ved=2ahUKEwiQupDN8OngAhUqCqYKHcSRCH84yAEQMyhAMEB6BAgBEEI&iact=mrc&uact=8',
   )
 musicians.save!
 
 Equipment_category.new(
-  name: 'Brass';
+  name: 'Brass'
   )
 equipment_category.save!
 Equipment_category.new(
