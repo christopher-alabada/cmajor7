d4f29e2ab3b4364369402043c68801f188e14694
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-07 16:58:20 +0900

    Added inclusion to requests.

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,7 @@ class Request < ApplicationRecord
   belongs_to :to, :class_name => 'User', :foreign_key => 'to_id', optional: true
 
   validates_uniqueness_of :from, :scope => [:to_id]
+
+  STATUSES = ['pending', 'accepted', 'rejected', 'confirmed']
+  validates :status, inclusion: { in: STATUSES, message: "%{value} is not a valid status" }
 end
