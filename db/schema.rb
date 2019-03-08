853a69329484073f3a9e75c3488a5a0eff4d95fc
Yuki Mori <masked04@gmail.com>
2019-03-08 10:15:39 +0900

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_03_07_065539) do
-=======
 ActiveRecord::Schema.define(version: 2019_03_07_124840) do
->>>>>>> 99809ba4873d3de687aa5f2c8eaf6abc8cafa1fc
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
