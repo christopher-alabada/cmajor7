e877a8ecd95f1493b8206241c1f62f545ecebf73
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-08 12:40:08 +0900

    Added some code.

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -2,8 +2,4 @@ class ChatRoomsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "chat_room_#{params[:chat_room_id]}"
   end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,19 @@
 class Message < ApplicationRecord
+  after_create :broadcast_message
+
   belongs_to :band
   belongs_to :from, :class_name => 'User', :foreign_key => 'from_id'
   belongs_to :to, :class_name => 'User', :foreign_key => 'to_id'
 
   validates :message, presence: true
+
+  def broadcast_message
+    ActionCable.server.broadcast("chat_room_#{chat_room.id}", {
+      message_partial: ApplicationController.renderer.render(
+        partial: "messages/message",
+        locals: { message: self, user_is_messages_author: false }
+      ),
+      current_user_id: user.id
+    })
+  end
 end
