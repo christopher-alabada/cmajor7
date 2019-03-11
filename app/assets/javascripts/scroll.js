ca31342f43d2a9973871e4b866104b1029015938
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-11 19:47:40 +0900

    Fixed chat issues

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,8 +1,5 @@
 function scrollLastMessageIntoView() {
   const messages = document.querySelectorAll('.message');
   const lastMessage = messages[messages.length - 1];
-
-  if (lastMessage !== undefined) {
-    lastMessage.scrollIntoView();
-  }
+  lastMessage.scrollIntoView();
 }
