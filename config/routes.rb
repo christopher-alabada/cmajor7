52792562854b5b248edeb66e9847e22fcbb688e2
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-12 11:31:19 +0900

    Added venue routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :musicians, only: [:index, :show, :new, :create]
   resources :bands, only: [:index, :show]
+  resources :venues, only: [:index, :show]
 
   post '/requests', to: 'requests#create'
   patch '/requests/:id', to: 'requests#update_status', as: 'edit_status_requests'
