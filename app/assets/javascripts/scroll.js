75e3864c6d551ae8abe6f0a5ba42793f4fb6ffd2
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-12 10:45:49 +0900

    Fixed chat bug and time.

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,5 +1,7 @@
 function scrollLastMessageIntoView() {
   const messages = document.querySelectorAll('.message');
   const lastMessage = messages[messages.length - 1];
-  lastMessage.scrollIntoView();
+  if (lastMessage) {
+    lastMessage.scrollIntoView();
+  }
 }
