32c98ca8c446d470391daee1228427822cc37bf7
Kumi Takenaka <kumireliy@gmail.com>
2019-03-14 12:02:27 +0900

    show badges in musician show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content, :stars)
+    params.require(:review).permit(:content, :stars, :badges)
   end
 end
