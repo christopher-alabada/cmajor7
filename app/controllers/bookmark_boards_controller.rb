caccab193584c05317f55c798f1b711daacabda9
Christopher Alabada <christopher.alabada@gmail.com>
2019-03-14 18:49:33 +0900

    Added automated chat message when adding someone to a band.

--- app/controllers/bookmark_boards_controller.rb
+++ app/controllers/bookmark_boards_controller.rb
@@ -12,8 +12,8 @@ class BookmarkBoardsController < ApplicationController
       # send automated message
       message = Message.new()
       message.user_id = current_user.id
-      message.band_id = @bookmark_board.band_id;
-      message.chat_room_id = @bookmark_board.band_id;
+      message.band_id = @bookmark_board.band_id
+      message.chat_room_id = @bookmark_board.band_id
       message.content = "** Added '#{Venue.find(@bookmark_board.venue_id).en_name}' to bookmarks. **"
       message.save
 
